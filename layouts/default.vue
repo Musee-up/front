f698ead872b790e4d1ac9c0b51e7f1bbf558a6b9
ygarrot <garrot.yoan1@gmail.com>
2022-04-01 14:18:52 +0200

    feat: testing nuxt bridge features

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,6 +3,7 @@
     <HomeNavbar />
     <!-- <v-container></v-container> -->
     <!-- <v-divider inset></v-divider> -->
+        <RegisterForm />
     <HomeFooter />
     <Nuxt />
   </div>
--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@
     <nuxt-img fit="cover" src="/home_bg.svg">
       <v-main fluid>
         <HomeSearchbar />
+        <RegisterForm />
       </v-main>
     </nuxt-img>
   </v-app>
