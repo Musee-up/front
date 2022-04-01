9d91f7b2c2fd9331e96b13b80556997d77fab2d0
ygarrot <garrot.yoan1@gmail.com>
2022-04-01 18:39:21 +0200

    feat: register component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,6 @@
 <template>
   <div>
     <HomeNavbar />
-    <!-- <v-container></v-container> -->
-    <!-- <v-divider inset></v-divider> -->
-    <RegisterForm />
     <HomeFooter />
     <Nuxt />
   </div>
