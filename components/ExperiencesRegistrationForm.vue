eebe100c9e51d2e27275f5365f289abf17083540
ygarrot <garrot.yoan1@gmail.com>
2022-04-06 17:28:55 +0200

    fix: lint

--- components/ExperiencesRegistrationForm.vue
+++ components/ExperiencesRegistrationForm.vue
@@ -1,14 +1,19 @@
 <template>
-  <v-card rounded class="d-flex flex-column rounded-xl align-center text-center" style="width:450px">
+  <v-card
+    rounded
+    class="d-flex flex-column rounded-xl align-center text-center"
+    style="width: 450px"
+  >
     <v-card-title>
-      <span class="text-h5 text-center"> {{ $t('À partir de 18€ par personne') }} </span>
+      <span class="text-h5 text-center">
+        {{ $t('À partir de 18€ par personne') }}
+      </span>
     </v-card-title>
     <v-card-text>
-      <DateMenu style="width:50px;"/>
+      <DateMenu style="width: 50px" />
     </v-card-text>
     <v-card-actions>
-      <base-blue-button text="Réserver"/>
+      <base-blue-button text="Réserver" />
     </v-card-actions>
   </v-card>
 </template>
-
--- pages/experiences/_id.vue
+++ pages/experiences/_id.vue
@@ -1,15 +1,15 @@
 <template>
   <v-container class="justify-center px-16" fluid>
     <v-row class="justify-center">
-      <v-col class=" text-left">
-        <h1 style="color:#24242C;font-size:40px;">
-          {{ experience.data.attributes.title }}</h1>
+      <v-col class="text-left">
+        <h1 style="color: #24242c; font-size: 40px">
+          {{ experience.data.attributes.title }}
+        </h1>
       </v-col>
     </v-row>
 
     <v-row class="justify-center">
-      <experience-group-slide
-        :photos="experience.data.attributes.photos"/>
+      <experience-group-slide :photos="experience.data.attributes.photos" />
     </v-row>
 
     <v-divider class="ma-9"></v-divider>
@@ -19,14 +19,9 @@
       <v-col>
         <v-list dense>
           <v-list-item-group>
-            <v-list-item
-              v-for="(item, i) in items"
-              :key="i"
-              >
+            <v-list-item v-for="(item, i) in items" :key="i">
               <v-list-item-icon>
-                <v-icon
-                  color="black"
-                  v-text="item.icon"></v-icon>
+                <v-icon color="black" v-text="item.icon"></v-icon>
               </v-list-item-icon>
               <v-list-item-content color="primary">
                 <v-list-item-title v-text="item.text"></v-list-item-title>
@@ -38,7 +33,7 @@
 
       <v-col>
         <v-row cols="2">
-        <ExperiencesRegistrationForm/>
+          <ExperiencesRegistrationForm />
         </v-row>
       </v-col>
     </v-row>
@@ -62,22 +57,34 @@ export default {
         return { id: parseInt(this.$route.params.id) }
       },
       update(data) {
-        const groupSizeSyntax = (groupSize) => `Jusqu'a ${groupSize} personnes (visite partagée)`
-      this.items = [
-        { text: data.experience.data.attributes.location, icon: 'mdi-map-marker' },
-        { text: data.experience.data.attributes.duration, icon: 'mdi-timer' },
-        { text: data.experience.data.attributes.transportation, icon: 'mdi-walk' },
-        { text: groupSizeSyntax(data.experience.data.attributes.groupSize), icon: 'mdi-account-group' },
-        //{ text: 'experience.data.attributes.', icon: 'mdi-camera' },
-        //{ text: 'experience.data.attributes.themes', icon: 'mdi-palette' },
-        //{ text: 'experience.data.attributes.languages', icon: 'mdi-earth' },
-      ]
+        const groupSizeSyntax = (groupSize) =>
+          `Jusqu'a ${groupSize} personnes (visite partagée)`
+        this.items = [
+          {
+            text: data.experience.data.attributes.location,
+            icon: 'mdi-map-marker',
+          },
+          { text: data.experience.data.attributes.duration, icon: 'mdi-timer' },
+          {
+            text: data.experience.data.attributes.transportation,
+            icon: 'mdi-walk',
+          },
+          {
+            text: groupSizeSyntax(data.experience.data.attributes.groupSize),
+            icon: 'mdi-account-group',
+          },
+          // { text: 'experience.data.attributes.', icon: 'mdi-camera' },
+          // { text: 'experience.data.attributes.themes', icon: 'mdi-palette' },
+          // { text: 'experience.data.attributes.languages', icon: 'mdi-earth' },
+        ]
         if (data.experience.data.attributes.handifriendly)
-          this.items.push({ text: 'Handifriendly', icon: 'mdi-human-wheelchair' })
+          this.items.push({
+            text: 'Handifriendly',
+            icon: 'mdi-human-wheelchair',
+          })
         return data.experience
-      }
+      },
     },
   },
 }
 </script>
-
