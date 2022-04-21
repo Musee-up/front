526ef0088da3843c90ba30735c9cba0cf0790685
ygarrot <garrot.yoan1@gmail.com>
2022-04-21 17:58:35 +0200

    fix: prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,7 @@ module.exports = {
   // add your custom rules here
   rules: {
     'no-console': 'off',
-    'vue/multi-word-component-names': 'off'
+    'vue/multi-word-component-names': 'off',
     // 'vue/multi-word-component-names': [
     //  'error',
     //  {
