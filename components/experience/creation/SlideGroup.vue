6c33ab2a007d5997453698074c4196fa92bbcf2e
ygarrot <garrot.yoan1@gmail.com>
2022-04-21 17:59:01 +0200

    feat: group slide

---
+++ components/experience/creation/SlideGroup.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-slide-group>
+    <v-slide-item>
+    </v-slide-item>
+    <v-slide-item>
+      <v-col class="d-flex align-content-space-between flex-wrap">
+        <v-row>
+        </v-row>
+        <v-row>
+        </v-row>
+      </v-col>
+    </v-slide-item>
+    <v-slide-item>
+    </v-slide-item>
+  </v-slide-group>
+</template>
+
