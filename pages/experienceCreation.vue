eb91ff74e41956c497b930ada0b68ad0faaa8337
ygarrot <garrot.yoan1@gmail.com>
2022-04-27 17:33:53 +0200

    fix: lint

--- pages/experienceCreation.vue
+++ pages/experienceCreation.vue
@@ -2,62 +2,62 @@
   <v-container
     class="justify-center"
     style="padding-right: 5%; padding-left: 5%"
-    >
-    <v-btn @click.prevent="createExperience"/>
+  >
+    <v-btn @click.prevent="createExperience" />
 
-      <v-row class="justify-center">
-        <v-col class="text-left">
-          <v-text-field
-            :label="
+    <v-row class="justify-center">
+      <v-col class="text-left">
+        <v-text-field
+          :label="
             $t(
-            'Indiquez le titre de votre expérience (maximum 40 caractères espaces compris)'
+              'Indiquez le titre de votre expérience (maximum 40 caractères espaces compris)'
             )
-            "
-            ></v-text-field>
-        </v-col>
-      </v-row>
+          "
+        ></v-text-field>
+      </v-col>
+    </v-row>
 
-      <v-row>
-        <v-container fluid>
-          <experience-creation-slide-group style="height: 100px" />
-        </v-container>
-      </v-row>
+    <v-row>
+      <v-container fluid>
+        <experience-creation-slide-group style="height: 100px" />
+      </v-container>
+    </v-row>
 
-      <v-divider class="my-9"></v-divider>
+    <v-divider class="my-9"></v-divider>
 
-      <v-row>
-        <v-col cols="8">
-          <experience-creation-attributes />
+    <v-row>
+      <v-col cols="8">
+        <experience-creation-attributes />
 
-            <v-divider class="my-9"></v-divider>
+        <v-divider class="my-9"></v-divider>
 
-            <v-row class="d-flex flex-row">
-              <h3 class="black--text py-4">
-                {{ $t("Présentation de l'experience") }}
-              </h3>
-            </v-row>
-            <v-row>
-              <v-text-field
-                v-model="title"
-                :label="
-                $t(
+        <v-row class="d-flex flex-row">
+          <h3 class="black--text py-4">
+            {{ $t("Présentation de l'experience") }}
+          </h3>
+        </v-row>
+        <v-row>
+          <v-text-field
+            v-model="title"
+            :label="
+              $t(
                 'Ecrivez-ici la présentation de votre expérience. (maximum 700 caractères espaces compris).'
-                )
-                "
-                ></v-text-field>
-            </v-row>
+              )
+            "
+          ></v-text-field>
+        </v-row>
 
-            <v-divider class="my-9"></v-divider>
+        <v-divider class="my-9"></v-divider>
 
-            <experience-guide-profile />
-        </v-col>
+        <experience-guide-profile />
+      </v-col>
 
-        <v-col>
-          <v-row class="justify-end">
-            <ExperienceRegistrationForm style="width: auto" />
-          </v-row>
-        </v-col>
-      </v-row>
+      <v-col>
+        <v-row class="justify-end">
+          <ExperienceRegistrationForm style="width: auto" />
+        </v-row>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -67,23 +67,22 @@ import experienceMutation from '@/graphql/mutations/experience'
 export default {
   data() {
     return {
-      title: "",
+      title: '',
     }
   },
   methods: {
-    async createExperience () {
+    async createExperience() {
       const result = await this.$apollo.mutate({
         mutation: experienceMutation,
         variables: {
           input: {
             title: this.title,
-            duration: "00:03:00.000",
-
-          }
-        }
+            duration: '00:03:00.000',
+          },
+        },
       })
       console.log(result)
-    }
-  }
+    },
+  },
 }
 </script>
